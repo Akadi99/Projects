c13c2df0e54fed94b05a716a961eb7ba8897ae05
Akadi99 <138606857+Akadi99@users.noreply.github.com>
2023-10-12 09:34:47 +0100

    Add files via upload
    Welcome to my Beginner project folder! You will find me working on basic codes from my journey of learning to code from quite a while back....
    
    Enjoy!!!

---
+++ Beginner/doginfo.py
@@ -0,0 +1,10 @@
+name = input("What is your dog's name? ")
+breed = input("What breed is your dog? ")
+age = int(input("How old is your dog? "))
+
+bark = True
+tweets = False
+
+print("My pet is called", name, ". He is a", breed, "and they are", age, "years old.")
+print("Statement:", name, "barks:", bark)
+print("Statement:", name, "tweets:", tweets)
---
+++ Beginner/formatstring.py
@@ -0,0 +1,5 @@
+num1 = 1.5
+num2 = 6.3
+name = "Joe"
+sum = num1 + num2
+print('Hi {0}. The sum of {1} and {2} is {3}.'.format(name, num1, num2, sum))
