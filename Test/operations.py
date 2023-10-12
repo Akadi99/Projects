dc737fef89108da28e8232fb12501fd46ac1d936
Akadi99 <138606857+Akadi99@users.noreply.github.com>
2023-10-12 09:29:01 +0100

    Add files via upload
    Welcome to my training coding. In this training folder, you will find basic codes from my journey of learning to become a developer. 
    
    Enjoyyyy!!!!!!

---
+++ Test/operations.py
@@ -0,0 +1,12 @@
+num1 = 50
+num2 = 20
+
+addition_result = num1 + num2
+subtract_result = num1 - num2
+division_result = num1 / num2
+multiply_result = num1 * num2
+
+print(addition_result)
+print(subtract_result)
+print(division_result)
+print(multiply_result)
